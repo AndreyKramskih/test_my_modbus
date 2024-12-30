93a55afa8b321d63d2b6f1bcc068f3553051ab42
Andrey Kramskih <andreykramskih@gmail.com>
2024-12-30 18:18:14 +0300

    Вставка значения ячейки из xlsx в docx

---
+++ xlsx _and_word.py
@@ -0,0 +1,13 @@
+
+from docx import Document
+import openpyxl
+
+doc = Document(docx = 'Инструкция-по-автоматике-Пороховой-ГВС-вода.docx')
+table=doc.tables[1]
+print(table.cell(1, 2).text)
+wb = openpyxl.load_workbook('Сетевые_Порох3_вода.xlsx')
+sheet = wb.active
+print(sheet['A1'].value)
+table.cell(1, 2).text= str(sheet['C25'].value)
+doc.save('Инструкция-по-автоматике-Пороховой-ГВС-вода.docx')
+print(table.cell(1, 2).text)
