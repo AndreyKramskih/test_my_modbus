33d4e147249bcfc01f79962df1e70d451292bef4
Andrey Kramskih <andreykramskih@gmail.com>
2024-12-30 18:31:58 +0300

    Вставка значения ячейки из xlsx в docx

--- xlsx _and_word.py
+++ xlsx _and_word.py
@@ -1,6 +1,7 @@
 
 from docx import Document
 import openpyxl
+from docx.enum.text import WD_ALIGN_PARAGRAPH
 
 doc = Document(docx = 'Инструкция-по-автоматике-Пороховой-ГВС-вода.docx')
 table=doc.tables[1]
@@ -9,5 +10,6 @@ wb = openpyxl.load_workbook('Сетевые_Порох3_вода.xlsx')
 sheet = wb.active
 print(sheet['A1'].value)
 table.cell(1, 2).text= str(sheet['C25'].value)
+table.cell(1, 2).paragraphs[0].alignment=WD_ALIGN_PARAGRAPH.RIGHT
 doc.save('Инструкция-по-автоматике-Пороховой-ГВС-вода.docx')
 print(table.cell(1, 2).text)
