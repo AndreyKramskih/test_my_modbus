43d2016ab61248d01d87e8def18996b3b9b37776
Andrey Kramskih <andreykramskih@gmail.com>
2025-01-02 17:32:34 +0300

    Изучение обработки событий при нажатии на кнопку

--- learn_tkinter.py
+++ learn_tkinter.py
@@ -1,8 +1,21 @@
 from tkinter import *
 
+clicks=0
+
 def finish():
     root.destroy()
     print('ручное закрыте окна')
+
+def click_button():
+    global clicks
+    clicks+=1
+    btn['text']=f'Нажали{clicks}раз'
+
+def entered(event):
+    btn['text']='Навел'
+
+def lefted(event):
+    btn['text']='Убрал'
 
 root=Tk()
 root.title('Приложение')
@@ -15,5 +28,11 @@ root.iconphoto(False,icon)
 label=Label(text='Привет')
 label.pack()
 root.protocol('WM_DELETE_WINDOW', finish)
+
+btn=Button(text='Жми', command=click_button)
+#btn.pack()
+btn.place(x=20,y=20)
+btn.bind('<Enter>', entered)
+btn.bind('<Leave>', lefted)
 
 root.mainloop()
